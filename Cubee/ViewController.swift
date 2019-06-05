80e09add9566531ccde671b86ec1a0af3c6889f9
tkielarbb <tomek.kielar00@gmail.com>
2019-06-05 22:57:32 +0200

    Initial Commit

---
+++ Cubee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cubee
+//
+//  Created by Tomasz Kielar on 05/06/2019.
+//  Copyright © 2019 Tomasz Kielar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
