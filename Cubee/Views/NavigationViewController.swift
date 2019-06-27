a1631b655bc9816cf10f9f25fc47e7f3be6192d6
Tomasz Kielar <tomek.kielar00@gmail.com>
2019-06-27 10:03:47 +0200

    -Changed to NavigationVC

---
+++ Cubee/Views/NavigationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationViewController.swift
+//  Cubee
+//
+//  Created by Tomasz Kielar on 27/06/2019.
+//  Copyright © 2019 Tomasz Kielar. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
