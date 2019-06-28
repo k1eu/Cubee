5e3f7d20b45fbda7090dc3b3d7fd326d832ebbd2
gjaworskibce <2000grzegorz@gmail.com>
2019-06-28 17:12:48 +0200

    -added avg of last 5 times counting

--- Cubee/TimesView.swift
+++ Cubee/TimesView.swift
@@ -22,6 +22,7 @@ class TimesView : UIViewController {
         updateUI()
         setBestTime()
         setLastTime()
+        setAvg()
     }
     
     func setBestTime() {
@@ -42,5 +43,44 @@ class TimesView : UIViewController {
         let savedTimes = defaults.stringArray(forKey: "times") ?? nil
         
         lastTimeLabel.text = "Last time\n" + (savedTimes?.last! ?? "---")
+    }
+    
+    func setAvg() {
+        let savedTimes = defaults.stringArray(forKey: "times") ?? nil
+        
+        var summaryOfTimes = 0
+        var avgText = "AVG of last 5\n---"
+        if savedTimes != nil && savedTimes!.count >= 5 {
+            for time in savedTimes!.suffix(5) {
+                //wyciaganie intow ze stringa
+                let minutes = Int(String(time.prefix(2)))
+                
+                let minutesAndSeconds = String(time.prefix(5))
+                let seconds = Int(String(minutesAndSeconds.suffix(2)))
+                
+                let hundreds = Int(String(time.suffix(2)))
+                
+                //zamienianie na setne
+                let minutesToHundreds = minutes! * 60 * 100
+                let secondsToHundreds = seconds! * 100
+                
+                //dodawanie wszystkiego do summaryOfTimes
+                summaryOfTimes += minutesToHundreds + secondsToHundreds + hundreds!
+            }
+            
+            //zamienianie na czas min:sec:set
+            summaryOfTimes /= 5
+            
+            let avgMinutes = summaryOfTimes/6000
+            let sumWithoutMinutes = summaryOfTimes%6000
+            
+            let avgSeconds = sumWithoutMinutes/100
+            let avgHundreds = sumWithoutMinutes%100
+            
+            //zapisywanie czasu jako string
+            avgText = "AVG of last 5\n" + String(format: "%02d:%02d.%02d", avgMinutes, avgSeconds, avgHundreds)
+        }
+        
+        avgLabel.text = avgText
     }
 }
