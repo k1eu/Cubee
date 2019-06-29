e873bb8ed6b0a478f4e927daf4448c1b3eb11cc7
gjaworskibce <2000grzegorz@gmail.com>
2019-06-29 12:31:03 +0200

    -added account photo picking (have to save it)

--- Cubee/AccountView.swift
+++ Cubee/AccountView.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class AccountView : UIViewController, UITableViewDelegate, UITableViewDataSource {
+class AccountView : UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var nickLabel: UILabel!
     @IBOutlet weak var accountImage: UIImageView!
     @IBOutlet weak var resultsButton: UIButton!
+    @IBOutlet weak var pickImageButton: UIButton!
     
     let defaults = UserDefaults.standard
+    var imagePicker = UIImagePickerController()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -26,6 +28,21 @@ class AccountView : UIViewController, UITableViewDelegate, UITableViewDataSource
         setTableView()
         print(defaults.stringArray(forKey: "times"))
     }
+    
+    //actions
+    @IBAction func pickImageAction(_ sender: Any) {
+            if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+                print("Button capture")
+                
+                imagePicker.delegate = self
+                imagePicker.sourceType = .savedPhotosAlbum
+                imagePicker.allowsEditing = false
+                
+                present(imagePicker, animated: true, completion: nil)
+            }
+        }
+        
+    
     
     //functions
     func setNickname() {
@@ -48,6 +65,27 @@ class AccountView : UIViewController, UITableViewDelegate, UITableViewDataSource
         
         accountImage.image = UIImage(named: actualImage ?? "account")
     }
+    
+    /*func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismiss(animated: true, completion: { () -> Void in
+            
+        })
+        print("weszlo")
+        accountImage.image = image
+        print("siema ", image)
+        defaults.set(image, forKey: "img")
+    }*/
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        print("weszlo")
+        guard let image = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        print("czy to to ", image)
+        accountImage.image = image
+    }
+    
     //tableview configuarion
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let savedTimes = defaults.stringArray(forKey: "times3x3") else {return 1}
@@ -66,5 +104,5 @@ class AccountView : UIViewController, UITableViewDelegate, UITableViewDataSource
         else {
             return cell
         }
-}
+    }
 }
